81a21de8915698f868cf73b2d70ffcda3895b629
Mohammed Sinan <mohammedsinan@bajajfinserv.in>
2024-10-11 15:52:29 +0530

    Added basic css and js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const shopNowBtn = document.getElementById('shopNowBtn');
+
+    shopNowBtn.addEventListener('click', () => {
+        alert('Redirecting to the shop page...');
+        // You can add the redirection logic here
+        // window.location.href = 'shop.html';
+    });
+});
